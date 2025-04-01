a9b522d243be3bdd2f6265cdc4a1f340f5fc4a21
HiteshDSoni <hiteshdsoni@yahoo.com>
2025-04-01 11:29:18 +0530

    Commit and push

---
+++ src/test/java/cartTests/Demo.java
@@ -0,0 +1,21 @@
+package cartTests;
+
+/**
+ * This is demo class
+ * 
+ * Time created	1 April 2025, 11:26:01 pm
+ * 
+ * Time last modified
+ * 
+ * */
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Demo Class");
+		
+
+	}
+
+}
